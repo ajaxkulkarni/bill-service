30697c2aba5b5ce33e3d12eb8ae1832d98c6ab54
ajaxkulkarni <ajinkyashiva@gmail.com>
2018-07-26 19:15:26 +0530

    changes for subscription deleted

--- code/billapp-service/src/main/java/com/rns/web/billapp/service/dao/impl/BillInvoiceDaoImpl.java
+++ code/billapp-service/src/main/java/com/rns/web/billapp/service/dao/impl/BillInvoiceDaoImpl.java
@@ -117,7 +117,7 @@ public class BillInvoiceDaoImpl {
 	}
 	
 	public List<Object[]> getCustomerInvoiceSummary(Date date, Integer businessId, Integer currentMonth, Integer currentYear) {
-		Query query = session.createQuery("select sum(invoice.amount),invoice.subscription,sum(invoice.pendingBalance),sum(invoice.serviceCharge),sum(invoice.creditBalance) from BillDBInvoice invoice where invoice.status!=:paid AND invoice.status!=:disabled AND (invoice.month!=:currentMonth OR  (invoice.month=:currentMonth AND invoice.year!=:currentYear) ) AND invoice.subscription!=:deleted AND invoice.subscription.business.id=:businessId group by invoice.subscription.id");
+		Query query = session.createQuery("select sum(invoice.amount),invoice.subscription,sum(invoice.pendingBalance),sum(invoice.serviceCharge),sum(invoice.creditBalance) from BillDBInvoice invoice where invoice.status!=:paid AND invoice.status!=:deleted AND (invoice.month!=:currentMonth OR  (invoice.month=:currentMonth AND invoice.year!=:currentYear) ) AND invoice.subscription!=:disabled AND invoice.subscription.business.id=:businessId group by invoice.subscription.id");
 		query.setString("paid", BillConstants.INVOICE_STATUS_PAID);
 		query.setInteger("businessId", businessId);
 		query.setInteger("currentMonth", currentMonth);
